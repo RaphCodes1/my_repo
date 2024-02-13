5c316279363b1a314e29239d54a15b1e598d5abd
Raphael <raphaelcreer@gmail.com>
2024-02-13 13:29:33 +0400

    new files

--- C02_proj/ex02.c
+++ C02_proj/ex02.c
@@ -1,12 +1,35 @@
 #include <unistd.h>
 #include <stdio.h>
 
-int ft_str_is_alpha(char *str)
+char ft_str_is_alpha(char *str)
 {
+    int i;
+    
+    i = 0;
 
+    while(str[i] != '\0')
+    {   
+        if((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z'))
+        {
+            continue;
+           
+        }else 
+        {
+            return(printf("0"));
+        }
+        i++;
+        
+    }   
+
+    return(printf("1"));
+
+    
 }
 
 int main(void)
 {
-    
+    char c[] = "abcdef";
+    ft_str_is_alpha(c);
+    printf("hello");
+
 }
