6a590538c7605e6a969bdfefb394616bef41f4a7
Raphael <raphaelcreer@gmail.com>
2024-02-20 13:44:45 +0400

    C05

--- C05/ex02/ft_iterative_power.c
+++ C05/ex02/ft_iterative_power.c
@@ -9,14 +9,7 @@ int ft_iterative_power(int nb, int power)
         return (0);
     if(nb == 0 || power == 0)
         return (1);
-    if(power == 2)
-        return (nb * 2);
-    while ((power - 1) <= nb)
-    {
-        result *= nb;
-        power--;
-    }
-    while(power >= nb)
+    while(power >= 1)
     {
         result *= nb;
         power--;
@@ -27,6 +20,6 @@ int ft_iterative_power(int nb, int power)
 
 int main(void)
 {
-    int  c = ft_iterative_power(3,2);
+    int  c = ft_iterative_power(10,3);
     printf("%d",c);
 }
